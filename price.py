15a6b36545628ddbb51e9f3a36ffb2ebabbf4648
Sasa Buklijas <buklijas.sasa@gmail.com>
2023-06-20 17:05:33 +0200

    US server bug fix, difference in % and UI improvment

--- price.py
+++ price.py
@@ -12,7 +12,12 @@ class BinancePrice:
     # https://binance-docs.github.io/apidocs/spot/en/#24hr-ticker-price-change-statistics
 
     def __init__(self):
-        response = urllib.request.urlopen(self._URL)
+        try:
+            response = urllib.request.urlopen(BinancePrice._URL)
+        except urllib.error.HTTPError:
+            BinancePrice._URL: str = 'https://api.binance.us/api/v3/ticker/24hr?symbols=["BTCUSDT"]'
+            response = urllib.request.urlopen(BinancePrice._URL)
+
         data = response.read().decode()
         json_data = json.loads(data)
 
@@ -43,24 +48,31 @@ def main():
 
     ps.row(
         ps.colxs(align='left', type='box', content=ps.h2(''))
-    +   ps.colxs(align='left', type='card', 
+    +   ps.colxs(align='right', type='card', 
                 content=ps.h2('Price for one Bitcoin in USDT'))
-    +   ps.colxs(align='left', type='card', 
+    +   ps.colxs(align='right', type='card', 
                 content=ps.h2('Satoshis (SATs) for one USDT'))
+    +   ps.colxs(align='center', type='card', 
+                content=ps.h2('Difference in %'))
     )
+    diff = bitcoin_price.current/bitcoin_price.last24h * 100 - 100
     ps.row(
         ps.colxs(align='right', type='card', content=ps.h2('Current:'))
-    +   ps.colxs(align='left', type='card', 
+    +   ps.colxs(align='right', type='card', 
                 content=ps.h2(f'{bitcoin_price.current:.2f} USDT'))
-    +   ps.colxs(align='left', type='card', 
+    +   ps.colxs(align='right', type='card', 
                 content=ps.h2(f'{100_000_000/bitcoin_price.current:.2f} SATs'))
+    +   ps.colxs(align='center', type='card', 
+                content=ps.h2(f'{diff:.2f} %')) # razlika
     )
     ps.row(
         ps.colxs(align='right', type='card', content=ps.h2('last 24h:'))
-    +   ps.colxs(align='left', type='card', 
+    +   ps.colxs(align='right', type='card', 
                 content=ps.h2(f'{bitcoin_price.last24h:.2f} USDT'))
-    +   ps.colxs(align='left', type='card', 
+    +   ps.colxs(align='right', type='card', 
                 content=ps.h2(f'{100_000_000/bitcoin_price.last24h:.2f} SATs'))
+    +   ps.colxs(align='center', type='card', 
+                content=ps.h2('---'))
     )
 
     ps.row(
