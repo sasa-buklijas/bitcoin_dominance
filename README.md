ab7d3f456141354bc7162f4d3700dc6f75c63731
Sasa Buklijas <buklijas.sasa@gmail.com>
2023-06-20 11:18:19 +0200

    added price.py

--- README.md
+++ README.md
@@ -16,4 +16,15 @@ Was not able to find it anywhere, so made my own.
 
 If you are looking fro bitcoin dominance over time chart "Total Market Capitalisation Dominance, %%" from [https://www.tradingview.com/markets/cryptocurrencies/global-charts/](https://www.tradingview.com/markets/cryptocurrencies/global-charts/) is useful.
 
+## price.py
 
+Bitcoin price dashboard of last and 24h weighted average price in USDT.
+
+### Example
+Available at: [http://buklijas.info/apps/bitcoin_price.html](http://buklijas.info/apps/bitcoin_price.html)
+
+### Usage
+python price.py \<HTML-file-output\>
+
+### Motivation
+For circular economy.
---
+++ price.py
@@ -0,0 +1,73 @@
+#import pprint
+import sys
+import urllib.request
+import json
+import piesparrow as ps
+from datetime import datetime
+
+
+class BinancePrice:
+    #_URL: str = 'https://api.binance.com/api/v3/ticker/24hr?symbol=BTCUSDT'
+    _URL: str = 'https://api.binance.com/api/v3/ticker/24hr?symbols=["BTCUSDT","BTCEUR"]'
+    # https://binance-docs.github.io/apidocs/spot/en/#24hr-ticker-price-change-statistics
+
+    def __init__(self):
+        response = urllib.request.urlopen(self._URL)
+        data = response.read().decode()
+        json_data = json.loads(data)
+
+        #pprint.pprint(json_data)
+
+        for asset in json_data:
+            if asset['symbol'] == 'BTCUSDT':
+                self.current = float(asset['lastPrice'])
+                self.last24h = float(asset['weightedAvgPrice'])
+
+
+def main():
+    # argv parsing
+    output_file = 'bitcoin_price'
+    if len(sys.argv) == 2 :
+        output_file = sys.argv[1]
+        
+    bitcoin_price = BinancePrice()
+
+    ps.init(filename = output_file, title = 'Bitcoin price')
+
+    generated_at = datetime.now()
+    time_zone = datetime.now().astimezone().tzname()
+    title = f'Generated at {generated_at} {time_zone}'
+    ps.row(
+        ps.colxl(type='box', content=ps.p(title))
+    )
+
+    ps.row(
+        ps.colxs(align='left', type='box', content=ps.h2(''))
+    +   ps.colxs(align='left', type='card', 
+                content=ps.h2('Price for one Bitcoin in USDT'))
+    +   ps.colxs(align='left', type='card', 
+                content=ps.h2('Satoshis (SATs) for one USDT'))
+    )
+    ps.row(
+        ps.colxs(align='right', type='card', content=ps.h2('Current:'))
+    +   ps.colxs(align='left', type='card', 
+                content=ps.h2(f'{bitcoin_price.current:.2f} USDT'))
+    +   ps.colxs(align='left', type='card', 
+                content=ps.h2(f'{100_000_000/bitcoin_price.current:.2f} SATs'))
+    )
+    ps.row(
+        ps.colxs(align='right', type='card', content=ps.h2('last 24h:'))
+    +   ps.colxs(align='left', type='card', 
+                content=ps.h2(f'{bitcoin_price.last24h:.2f} USDT'))
+    +   ps.colxs(align='left', type='card', 
+                content=ps.h2(f'{100_000_000/bitcoin_price.last24h:.2f} SATs'))
+    )
+
+    ps.row(
+        ps.colxl(align='center', type='box', content=ps.link('https://github.com/sasa-buklijas/bitcoin_dominance',
+                                                            'Code available on GitHub'))
+    )
+
+
+if __name__ == '__main__':
+	main()
